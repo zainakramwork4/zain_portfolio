98d95659a887b72f72728e4620b3721f830097d5
Muhammad Zain Akram <zainakram.work4@gmail.com>
2026-07-20 15:32:27 +0500

    Update NavButton.jsx

--- src/components/navigation/NavButton.jsx
+++ src/components/navigation/NavButton.jsx
@@ -79,7 +79,7 @@ const NavButton = ({
 
                 <span className="peer bg-transparent absolute top-0 left-0 w-full h-full" />
 
-                <span className="absolute hidden peer-hover:block px-2 py-1 left-full mx-2 top-1/2 -translate-y-1/2 bg-background text-foreground text-sm rounded-md shadow-lg whitespace-nowrap">
+               <span className="relative w-14 h-14 p-4 animate-spin-slow-reverse group-hover:[animation-play-state:paused] hover:text-accent">
                   {label}
                 </span>
               </span>
