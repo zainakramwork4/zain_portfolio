47966b3ce5e3363ba7ea600fb4c1000c3fad8d6d
Muhammad Zain Akram <zainakram.work4@gmail.com>
2026-07-20 16:00:20 +0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,35 +7,36 @@ module.exports = {
   ],
   theme: {
     extend: {
-      fontFamily:{
-        inter: ['var(--font-inter)']
+      fontFamily: {
+        inter: ["var(--font-inter)"],
       },
-      colors:{
-        background: 'rgb(var(--background))',
-        foreground: 'rgb(var(--foreground))',
-        muted: 'rgb(var(--muted))',
-        accent: 'rgb(var(--accent))',
+      colors: {
+        background: "rgb(var(--background))",
+        foreground: "rgb(var(--foreground))",
+        muted: "rgb(var(--muted))",
+        accent: "rgb(var(--accent))",
       },
-      backgroundImage:{
-        'firefly-radial': "radial-gradient(50% 50% at 50% 50%, rgba(253, 255, 80, 0.5) 0%, rgba(217,217,217, 0) 100%)"
+      backgroundImage: {
+        "firefly-radial":
+          "radial-gradient(50% 50% at 50% 50%, rgba(253, 255, 80, 0.5) 0%, rgba(217,217,217, 0) 100%)",
       },
-      boxShadow:{
-        'glass-inset': 'inset 0 17px 5px -9px rgba(254,254,91, 0.05)',
-        'glass-sm': '5px 5px 20px 0px rgba(254,254,91, 0.3)',
+      boxShadow: {
+        "glass-inset": "inset 0 17px 5px -9px rgba(254,254,91, 0.05)",
+        "glass-sm": "5px 5px 20px 0px rgba(254,254,91, 0.3)",
       },
-      keyframes:{
-      'spin-reverse':{
-        '0%': {transform: 'rotate(0deg)'},
-        '100%': {transform: 'rotate(-360deg)'}
-      }
+      keyframes: {
+        "spin-reverse": {
+          "0%": { transform: "rotate(0deg)" },
+          "100%": { transform: "rotate(-360deg)" },
+        },
       },
-      animation:{
-        'spin-slow': 'spin 40s linear infinite',
-        'spin-slow-reverse': 'spin-reverse 40s linear infinite',
+      animation: {
+        "spin-slow": "spin 40s linear infinite",
+        "spin-slow-reverse": "spin-reverse 40s linear infinite",
       },
-      screens:{
-        xs: '480px',
-      }
+      screens: {
+        xs: "480px",
+      },
     },
   },
   plugins: [],
