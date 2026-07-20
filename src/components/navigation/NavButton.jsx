e3a6c5f9999be5528cd0eada0991e455e0d1a614
Muhammad Zain Akram <zainakram.work4@gmail.com>
2026-07-20 19:59:14 +0500

    Update NavButton.jsx

--- src/components/navigation/NavButton.jsx
+++ src/components/navigation/NavButton.jsx
@@ -18,18 +18,25 @@ const getIcon = (icon) => {
   switch (icon) {
     case "home":
       return <Home className="w-full h-auto" strokeWidth={1.5} />;
+
     case "about":
       return <User className="w-full h-auto" strokeWidth={1.5} />;
+
     case "projects":
       return <Palette className="w-full h-auto" strokeWidth={1.5} />;
+
     case "contact":
       return <Phone className="w-full h-auto" strokeWidth={1.5} />;
+
     case "github":
       return <Github className="w-full h-auto" strokeWidth={1.5} />;
+
     case "linkedin":
       return <Linkedin className="w-full h-auto" strokeWidth={1.5} />;
+
     case "twitter":
       return <Twitter className="w-full h-auto" strokeWidth={1.5} />;
+
     case "resume":
       return <NotebookText className="w-full h-auto" strokeWidth={1.5} />;
 
@@ -53,56 +60,57 @@ const NavButton = ({
   icon,
   newTab,
   labelDirection = "right",
+  paused,
+  setPaused,
 }) => {
   return (
     <ResponsiveComponent>
       {({ size }) => {
         return size && size >= 480 ? (
-          /* Desktop Version */
           <div
             className="absolute cursor-pointer z-50"
             style={{ transform: `translate(${x}, ${y})` }}
+            onMouseEnter={() => setPaused?.(true)}
+            onMouseLeave={() => setPaused?.(false)}
           >
             <NavLink
               variants={item}
               href={link}
               target={newTab ? "_blank" : "_self"}
-              className="group text-foreground rounded-full flex items-center justify-center custom-bg"
+              className="text-foreground rounded-full flex items-center justify-center custom-bg"
               aria-label={label}
               name={label}
               prefetch={false}
               scroll={false}
             >
-              <span className="relative w-14 h-14 p-4 animate-spin-slow-reverse group-hover:[animation-play-state:paused] hover:text-accent">
+              <span
+                className={`relative w-14 h-14 p-4 animate-spin-slow-reverse ${
+                  paused ? "pause" : ""
+                } hover:text-accent`}
+              >
                 {getIcon(icon)}
 
                 <span className="peer bg-transparent absolute top-0 left-0 w-full h-full" />
 
-                <span
-                  className={clsx(
-                    "absolute hidden peer-hover:block px-2 py-1 left-full mx-2 top-1/2 -translate-y-1/2 bg-background text-foreground text-sm rounded-md shadow-lg whitespace-nowrap",
-                    labelDirection === "left" ? "right-full left-auto mx-2" : ""
-                  )}
-                >
+                <span className="absolute hidden peer-hover:block px-2 py-1 left-full mx-2 top-1/2 -translate-y-1/2 bg-background text-foreground text-sm rounded-md shadow-lg whitespace-nowrap">
                   {label}
                 </span>
               </span>
             </NavLink>
           </div>
         ) : (
-          /* Mobile / Small Screens Version */
           <div className="w-fit cursor-pointer z-50">
             <NavLink
               variants={item}
               href={link}
               target={newTab ? "_blank" : "_self"}
-              className="group text-foreground rounded-full flex items-center justify-center custom-bg"
+              className="text-foreground rounded-full flex items-center justify-center custom-bg"
               aria-label={label}
               name={label}
               prefetch={false}
               scroll={false}
             >
-              <span className="relative w-10 h-10 xs:w-14 xs:h-14 p-2.5 xs:p-4 animate-spin-slow-reverse group-hover:[animation-play-state:paused] hover:text-accent">
+              <span className="relative w-10 h-10 xs:w-14 xs:h-14 p-2.5 xs:p-4 hover:text-accent">
                 {getIcon(icon)}
 
                 <span className="peer bg-transparent absolute top-0 left-0 w-full h-full" />
@@ -110,7 +118,7 @@ const NavButton = ({
                 <span
                   className={clsx(
                     "absolute hidden peer-hover:block px-2 py-1 left-full mx-2 top-1/2 -translate-y-1/2 bg-background text-foreground text-sm rounded-md shadow-lg whitespace-nowrap",
-                    labelDirection === "left" ? "right-full left-auto mx-2" : ""
+                    labelDirection === "left" && "right-full left-auto"
                   )}
                 >
                   {label}
